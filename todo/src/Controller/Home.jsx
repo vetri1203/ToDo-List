4fb2ed9f8b999515614290674163a37061a8e881
vetri1203 <vetriveljaganathan5125@gmail.com>
2023-12-20 09:19:57 +0530

    inital setup done

---
+++ todo/src/Controller/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+  return (
+      <div className='Container'>
+          <h2 className='webname'>Task Manager</h2>
+
+    </div>
+  )
+}
+
+export default Home
